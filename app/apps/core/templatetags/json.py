0f9441c10d81bdeabef9295d839f4aee8edbcb80
yvesconst <yvesconst@yahoo.fr>
2021-10-02 10:57:45 +0200

    Optimize metrics computation

--- app/apps/core/templatetags/json.py
+++ app/apps/core/templatetags/json.py
@@ -8,8 +8,3 @@ register = template.Library()
 @register.filter
 def jsond(obj):
     return json.dumps(obj)
-
-@register.filter
-def parsedict(obj):
-    list_data = [v for k,v in obj.items()]
-    return list_data
