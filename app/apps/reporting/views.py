064f4904ebef91e93c0ccec5bac5a892358d4c1d
Eva Bardou <ebardou@teklia.com>
2021-10-25 15:33:20 +0200

    Reduce queries number in Leaderboard + Nit

--- app/apps/reporting/views.py
+++ app/apps/reporting/views.py
@@ -71,7 +71,6 @@ class QuotasLeaderboard(LoginRequiredMixin, ListView):
     def get_queryset(self):
         qs = super().get_queryset()
         today = date.today()
-        filter_last_month = Q(taskreport__started_at__gte=today - timedelta(days=30))
         filter_last_week = Q(taskreport__started_at__gte=today - timedelta(days=7))
         filter_last_day = Q(taskreport__started_at__gte=today - timedelta(days=1))
         runtime = ExpressionWrapper(
