a2ac1247dbeb69ca4ae4f910a7d5ac8300f01b6f
Eva Bardou <ebardou@teklia.com>
2021-10-25 16:24:21 +0200

    Fix some review related code snippets

--- app/apps/core/management/commands/index.py
+++ app/apps/core/management/commands/index.py
@@ -1,6 +1,3 @@
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk as es_bulk
-
 from django.core.management.base import BaseCommand
 from core.search import Indexer
 from core.models import Project
@@ -10,11 +7,7 @@ class Command(BaseCommand):
     help = 'Index all projects by creating a DocumentPart wide transcription for each Document in each Project.'
         
     def handle(self, *args, **options):
-        es_client = Elasticsearch()
-
         for project in Project.objects.all():
             indexer = Indexer(project)
-            to_insert = indexer.process()
-
-            nb_inserted, _ = es_bulk(es_client, to_insert, stats_only=True)
-            print(f"Inserted {nb_inserted} new entries in index {indexer.index_name}")
+            indexer.configure()
+            indexer.process()
--- app/apps/core/search.py
+++ app/apps/core/search.py
@@ -1,8 +1,24 @@
+from elasticsearch import Elasticsearch
+from elasticsearch.client import IndicesClient
+from elasticsearch.helpers import bulk as es_bulk
+
+from django.conf import settings
+
+ES_HOST = settings.ELASTICSEARCH_HOST + ":" + settings.ELASTICSEARCH_PORT
+
+
 class Indexer:
+    es_client = Elasticsearch(hosts=[ES_HOST])
 
     def __init__(self, project):
         self.project = project
         self.index_name = f"project-{project.id}"
+
+    def configure(self):
+        indices = IndicesClient(self.es_client)
+        if not indices.exists(self.index_name):
+            indices.create(self.index_name)
+            print(f"Created a new index named {self.index_name}")
 
     def process(self):
         to_insert = []
@@ -11,7 +27,8 @@ class Indexer:
 
         to_insert = [entry for entry in to_insert if entry["transcription"]]
 
-        return to_insert
+        nb_inserted, _ = es_bulk(self.es_client, to_insert, stats_only=True)
+        print(f"Inserted {nb_inserted} new entries in index {self.index_name}")
 
     def process_document(self, document):
         return [
