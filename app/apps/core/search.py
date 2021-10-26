4df102853c59851b43b8b03c059f16c83d2eecb0
Eva Bardou <ebardou@teklia.com>
2021-10-26 11:45:21 +0200

    Create a common index for all data instead of one per project

--- app/apps/core/search.py
+++ app/apps/core/search.py
@@ -4,6 +4,7 @@ from elasticsearch.helpers import bulk as es_bulk
 
 from django.conf import settings
 
+COMMON_INDEX = "escriptorium-data"
 ES_HOST = settings.ELASTICSEARCH_HOST + ":" + settings.ELASTICSEARCH_PORT
 
 
@@ -12,13 +13,12 @@ class Indexer:
 
     def __init__(self, project):
         self.project = project
-        self.index_name = f"project-{project.id}"
 
     def configure(self):
         indices = IndicesClient(self.es_client)
-        if not indices.exists(self.index_name):
-            indices.create(self.index_name)
-            print(f"Created a new index named {self.index_name}")
+        if not indices.exists(COMMON_INDEX):
+            indices.create(COMMON_INDEX)
+            print(f"Created a new index named {COMMON_INDEX}")
 
     def process(self):
         to_insert = []
@@ -28,16 +28,16 @@ class Indexer:
         to_insert = [entry for entry in to_insert if entry["transcription"]]
 
         nb_inserted, _ = es_bulk(self.es_client, to_insert, stats_only=True)
-        print(f"Inserted {nb_inserted} new entries in index {self.index_name}")
+        print(f"Inserted {nb_inserted} new entries in index {COMMON_INDEX}")
 
     def process_document(self, document):
         return [
             {
-                "_index": self.index_name,
+                "_index": COMMON_INDEX,
                 "_type": "document",
                 "_id": str(part.id),
                 "document_id": str(document.id),
-                "project_slug": str(self.project.slug),
+                "project_id": str(self.project.id),
                 "transcription": " ".join([transcription.content for line in part.lines.all() for transcription in line.transcriptions.all()])
             }
             for part in document.parts.all()
