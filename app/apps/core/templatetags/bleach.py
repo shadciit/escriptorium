0e1c2ff9e4bfd9845199b0efad13b72ae5608746
yvesconst <yvesconst@yahoo.fr>
2021-11-18 16:32:41 +0100

    New template for pagination

--- app/apps/core/templatetags/bleach.py
+++ app/apps/core/templatetags/bleach.py
@@ -9,3 +9,7 @@ register = template.Library()
 def strip_html(obj, tags=None):
     tags = tags or None
     return bleach.clean(obj, strip=True, tags=tags)
+
+@register.filter()
+def format_tags(tags=None):
+    return ('&' + '&'.join([ ('tags=' + tag) for tag in tags])) if tags else ''
