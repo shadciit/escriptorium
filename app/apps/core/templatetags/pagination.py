941858dd4f7e97b52384834014963bcd6473d094
yvesconst <yvesconst@yahoo.fr>
2021-11-24 18:54:02 +0100

    Fix links on pagination

--- app/apps/core/templatetags/pagination.py
+++ app/apps/core/templatetags/pagination.py
@@ -8,7 +8,3 @@ def url_replace(request, field, value):
     dict_ = request.GET.copy()
     dict_[field] = value
     return dict_.urlencode()
-
-@register.filter()
-def format_tags(tags=None):
-    return ('&' + '&'.join([ ('tags=' + tag) for tag in tags])) if tags else ''
