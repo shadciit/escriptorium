3f4b2478d96d57b4b59d0b16d8c36f9301dd1971
Itay Zandbank <itay@chelem.co.il>
2022-01-10 17:55:50 +0200

    Add dev settings for running a dev environment inside docker

---
+++ app/escriptorium/docker_dev_settings.py
@@ -0,0 +1,43 @@
+# Settings for running the app in development mode inside a Docker Container
+
+from escriptorium.settings import *
+
+INSTALLED_APPS += ['debug_toolbar', 'django_extensions']
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
+INTERNAL_IPS = ['127.0.0.1',]
+
+# only needed in development
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/django-emails'
+
+# KRAKEN_TRAINING_DEVICE = 'cuda:0'
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+#   'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+# Explicitly set the directories
+BASE_DIR = '/usr/src/app'
+APPS_DIR = os.path.join(BASE_DIR, 'apps')
+if APPS_DIR not in sys.path:
+    sys.path.append(APPS_DIR)
+
+DEBUG = True
+
+FRONTEND_DIR = '/usr/src/front/dist/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+    FRONTEND_DIR
+]
