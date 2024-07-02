b173c3c84a34676fa87c5503888f9b741712bc88
Robin Tissot <tissotrobin@gmail.com>
2024-07-02 15:48:56 +0200

    Fixes changing number of elements in a paginated page showing http 404. Closes #1077.

--- app/apps/core/templatetags/pagination.py
+++ app/apps/core/templatetags/pagination.py
@@ -4,7 +4,8 @@ register = template.Library()
 
 
 @register.simple_tag
-def url_replace(request, field, value):
+def url_replace(request, *url_parameters):
     dict_ = request.GET.copy()
-    dict_[field] = value
+    for field, value in zip(url_parameters[::2], url_parameters[1::2]):
+        dict_[field] = value
     return dict_.urlencode()
